e20873eac5136f2bdfac23209fb09a6e2f951e4e
james chahwan <j.r.chahwan@gmail.com>
2014-09-27 23:08:11 +1000

    School names added and user privileges have been checked.

--- DetentionTrackerAIP/create_login_db.sql
+++ DetentionTrackerAIP/create_login_db.sql
@@ -1,6 +1,14 @@
+CREATE TABLE PERMISSIONS
+(
+SCHOOLNAME varchar(255)not null primary key,
+Userpriveliege integer DEFAULT 1
+);
+
 CREATE TABLE logins
 (
 Username varchar(255),
 Password varchar(255),
-Userpriveliege integer
+SCHOOLNAME VARCHAR(255) REFERENCES PERMISSIONS(SCHOOLNAME)
 );
+
+
