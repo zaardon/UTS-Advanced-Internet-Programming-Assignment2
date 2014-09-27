51b7edc629381c1468733a8007f37c40326091d5
james chahwan <j.r.chahwan@gmail.com>
2014-09-27 23:09:55 +1000

    detentiontracker table has been updated for SCHOOLNAME use

--- DetentionTrackerAIP/create_detentions_db.sql
+++ DetentionTrackerAIP/create_detentions_db.sql
@@ -6,5 +6,6 @@ LastName varchar(255),
 YearGroup int,
 DetentionType varchar(255),
 Department varchar(255),
-Reason varchar(255)
+Reason varchar(255),
+SCHOOLNAME VARCHAR(255) REFERENCES PERMISSIONS(SCHOOLNAME)
 );
