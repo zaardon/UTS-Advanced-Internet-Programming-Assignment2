1b3efca36403f9b8743d2dcfa415dc68fb8a1915
Alex Smith <smith.alexanderjames@gmail.com>
2014-10-18 16:45:39 +1100

    Fixed Source Package Bug in WAR

--- DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Detention_.java
+++ DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Detention_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-18T16:19:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-18T16:44:14")
 @StaticMetamodel(Detention.class)
 public class Detention_ { 
 
--- DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Receipt_.java
+++ DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Receipt_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-18T16:19:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-18T16:44:14")
 @StaticMetamodel(Receipt.class)
 public class Receipt_ { 
 
