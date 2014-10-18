bd70af43deca55a54e240ce3813cb32c63523f7e
Alex Smith <smith.alexanderjames@gmail.com>
2014-10-18 16:24:42 +1100

    XHTML Page Edits.

---
+++ DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Login_.java
@@ -0,0 +1,21 @@
+package au.edu.uts.aip.detentiontracker.domain;
+
+import au.edu.uts.aip.detentiontracker.domain.AccountType;
+import au.edu.uts.aip.detentiontracker.domain.Detention;
+import au.edu.uts.aip.detentiontracker.domain.Receipt;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-18T16:19:06")
+@StaticMetamodel(Login.class)
+public class Login_ { 
+
+    public static volatile ListAttribute<Login, Receipt> receipts;
+    public static volatile SingularAttribute<Login, String> password;
+    public static volatile ListAttribute<Login, Detention> detentions;
+    public static volatile SingularAttribute<Login, AccountType> accountType;
+    public static volatile SingularAttribute<Login, String> username;
+
+}
