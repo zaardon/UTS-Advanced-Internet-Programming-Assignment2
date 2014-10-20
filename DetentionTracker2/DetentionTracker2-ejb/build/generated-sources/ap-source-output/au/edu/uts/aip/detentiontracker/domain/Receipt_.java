de41b0252143f23ceeba64c18f83a6f715d11042
Alex Smith <smith.alexanderjames@gmail.com>
2014-10-20 17:33:40 +1100

    Account Management option at the bottom of page

--- DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Receipt_.java
+++ DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Receipt_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-20T16:24:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-10-20T17:28:20")
 @StaticMetamodel(Receipt.class)
 public class Receipt_ { 
 
