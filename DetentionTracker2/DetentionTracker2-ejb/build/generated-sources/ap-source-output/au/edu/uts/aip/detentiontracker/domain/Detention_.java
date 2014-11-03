887de56ad508b658800d23b6186447e36efea70a
james chahwan <j.r.chahwan@gmail.com>
2014-11-03 18:27:21 +1100

    Bootstrap UI
    EVERYTHING UI IS DONE

--- DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Detention_.java
+++ DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Detention_.java
@@ -8,17 +8,17 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-03T17:07:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-03T17:17:12")
 @StaticMetamodel(Detention.class)
 public class Detention_ { 
 
-    public static volatile SingularAttribute<Detention, String> reason;
-    public static volatile SingularAttribute<Detention, String> lastName;
-    public static volatile SingularAttribute<Detention, String> firstName;
     public static volatile SingularAttribute<Detention, DetentionType> detentionType;
+    public static volatile SingularAttribute<Detention, String> lastName;
+    public static volatile SingularAttribute<Detention, YearType> yearType;
+    public static volatile SingularAttribute<Detention, String> reason;
     public static volatile SingularAttribute<Detention, Integer> detentionID;
     public static volatile SingularAttribute<Detention, Login> login;
+    public static volatile SingularAttribute<Detention, String> firstName;
     public static volatile SingularAttribute<Detention, DepartmentType> departmentType;
-    public static volatile SingularAttribute<Detention, YearType> yearType;
 
 }
--- DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Login_.java
+++ DetentionTracker2/DetentionTracker2-ejb/build/generated-sources/ap-source-output/au/edu/uts/aip/detentiontracker/domain/Login_.java
@@ -8,16 +8,16 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-03T17:07:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-03T17:17:12")
 @StaticMetamodel(Login.class)
 public class Login_ { 
 
-    public static volatile ListAttribute<Login, Receipt> receipts;
-    public static volatile SingularAttribute<Login, String> password;
-    public static volatile ListAttribute<Login, Detention> detentions;
-    public static volatile SingularAttribute<Login, AccountType> accountType;
-    public static volatile SingularAttribute<Login, String> email;
     public static volatile SingularAttribute<Login, String> username;
     public static volatile SingularAttribute<Login, String> token;
+    public static volatile SingularAttribute<Login, String> email;
+    public static volatile ListAttribute<Login, Receipt> receipts;
+    public static volatile SingularAttribute<Login, AccountType> accountType;
+    public static volatile SingularAttribute<Login, String> password;
+    public static volatile ListAttribute<Login, Detention> detentions;
 
 }
